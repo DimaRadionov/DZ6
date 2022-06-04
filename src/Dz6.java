43c2229619af9838d7c44641b332da9aba513317
DimaRadionov <104160691+DimaRadionov@users.noreply.github.com>
2022-06-04 16:20:00 +0200

    Update Dz6.java

--- src/Dz6.java
+++ src/Dz6.java
@@ -1,116 +1,13 @@
 import javax.sound.midi.Soundbank;
 import java.sql.SQLOutput;
 
-public class Dz6{
+public class DZ6 {
 
     public static void main(String[] args) {
-
-        // Операция сложения
-
-        int a = 10;
-        int b = 8;
-        int c = a + b;
-        int d = 4 + b;
-
-        System.out.println(c);
-        System.out.println(d);
-
-        // Операция вычесления
-
-         a = 10;
-         b = 8;
-         c = a - b;
-         d = 4 - a;
-
-        System.out.println(c);
-        System.out.println(d);
-
-        // Операция умножения
-
-         a = 10;
-         b = 8;
-         c = a * b;
-         d = b * 5;
-
-
-        System.out.println(c);
-        System.out.println(d);
-
-        // Операция деления
-
-        int number_One = 20;
-        int number_Two = 4;
-        int number_Three = a / b;
-        double number_Four = 22.5 / 4.5;
-
-
-        System.out.println(number_Three);
-        System.out.println(number_Four);
-
-
-        double k = 20 / 4;
-
-        System.out.println(k);
-
-
-        double f = 20.0 / 4;
-
-        System.out.println(f);
-
-        // Получение остатка от деления двух чисел
-
-         a = 33;
-         b = 5;
-         c = a % b;
-         d = 22 % 4;
-
-        System.out.println(c);
-        System.out.println(d);
-
-        // ++ (префиксный инкремент)
-
-         a = 8;
-         b = ++a;
-        System.out.println(a);
-        System.out.println(b);
-
-        // ++ (постфиксный  инкремент)
-
-         a = 8;
-         b = a++;
-
-        System.out.println(a);
-        System.out.println(b);
-
-        // -- (префиксный декремент)
-
-         a = 8;
-         b = --a;
-        System.out.println(a);
-        System.out.println(b);
-
-        // -- (постфиксный декремент)
-
-         a = 8;
-         b = a--;
-        System.out.println(a);
-        System.out.println(b);
-
-         a = 8;
-         b = 7;
-         c = a + 5 * ++b;
-
-        System.out.println(c);
-
-         a = 8;
-         b = 7;
-         c = (a + 5) * ++b;
-        System.out.println(c);
-
-        int x = 10 / 5 * 2;
-        System.out.println(x);
-
-        double number = 2.0 - 1.1;
-        System.out.println(number);
+        int a = 20;
+        int b = 10;
+        int c = 9;
+        double result = Math.abs(a - b) / Math.pow((a + b),3) - Math.sqrt(c);
+        System.out.println("Result is: " + result);
     }
 }
